b68de3cf377e5d145c04fbfb344f403315890836
Ridho Aryo Bimo <ridho.bimo@students.amikom.ac.id>
2018-12-09 13:18:32 +0700

    PWL

--- pertemuan8/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $hostname  = "localhost";
-   $username  = "root";
-   $password  = "";
-   $dbname  = "ridhoaryobimo";
-   $db = new mysqli($hostname, $username, $password, $dbname);
-?>
--- pertemuan8/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-   session_start();
-   session_destroy();
-?>
-
-<div align="center">
-  <h2>Anda telah berhasil logout..</h2>
-  Silahkan klik <a href="login.php">disini</a> untuk login kembali
-</div>
