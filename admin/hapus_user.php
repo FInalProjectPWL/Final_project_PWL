024ce79cfabc9f5a1d1d3871cabd8ac103d8966f
Ridho Aryo Bimo <ridho.bimo@students.amikom.ac.id>
2018-12-16 16:50:18 +0700

    del

--- admin/hapus_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "koneksi.php";
-	$id_user = $_GET['id_user'];
-	
-	$query = mysqli_query($connection,"DELETE FROM user WHERE id_user='$id_user'");
-
-	header("location:view_user.php");
-?>
